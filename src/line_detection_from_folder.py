2324a0abe345d7f284189643b4886888746394f0
jetbot <mordredur@googlemail.com>
2020-06-15 17:16:56 +0200

    cpu prediction

--- src/line_detection_from_folder.py
+++ src/line_detection_from_folder.py
@@ -12,8 +12,6 @@ class Line_Detection:
     def predict(self, image_tensor):
         prediction = self.model(image_tensor)
         preds, indices = torch.topk(prediction, 1)
-        print("Indices:", indices)
-        print("Preds:", prediction)
         pred_class = self.class_names[indices.flatten().item()]
         return pred_class
 
@@ -24,6 +22,7 @@ class Line_Detection:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         tensor = torch.from_numpy(image)
         tensor = tensor.to(self.device)
+        #tensor = tensor.cuda()
         tensor = tensor[None]
         tensor = tensor.permute(0,3,1,2)
         tensor = tensor.float()
@@ -34,14 +33,15 @@ class Line_Detection:
         use_cuda = not torch.cuda.is_available()
         self.device = torch.device("cuda" if use_cuda else "cpu")
         self.model = torch.load(path)
-        self.model.to(self.device)
+        #self.model = self.model.cuda()
+        self.model = self.model.to(self.device)
         self.model.eval()
 
 
 def main():
     image_paths = glob.glob("/home/jetbot/Pictures/*.jpg")
 
-    path = "./models/resnet34_97acc.pth" 
+    path = "./models/resnet18_95acc.pth" 
     ld = Line_Detection(path)
     #camera = CSICamera(width=224, height=224, capture_width=224, capture_height=224, capture_fps=21)
     for idx, img in enumerate(image_paths):
@@ -50,7 +50,6 @@ def main():
         tensor = ld.preprocess(image)
         pred_class = ld.predict(tensor)
         print(img, idx, pred_class)
-        
 
 if __name__=="__main__":
     main()
