91ea20d0e13a905d1cb006bf489690a970bd9b9d
jetbot <mordredur@googlemail.com>
2020-06-16 11:39:53 +0200

    adds transform for correct pred

--- src/line_detection.py
+++ src/line_detection.py
@@ -1,29 +1,33 @@
+#!/usr/bin/env python3
+
 import sys
 sys.path.append("~/jetcam/jetcam")
 
 import time
 import cv2
 import torch 
-
+import glob
+from torchvision import transforms
 from jetcam.csi_camera import CSICamera
 
 class Line_Detection:
 
     def predict(self, image_tensor):
-        prediction = self.model(image_tensor)
-        __, indices = torch.topk(prediction, 1)
-        pred_class = self.class_names[indices.flatten().item()]
-        return pred_class
+        with torch.no_grad():
+            prediction = self.model(image_tensor)
+            preds, indices = torch.max(prediction, 1)
+            pred_class = self.class_names[indices.flatten().item()]
+            return pred_class
 
     def get_device(self):
         return self.device
 
     def preprocess(self, image):
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        tensor = torch.from_numpy(image)
+        transform = transforms.Compose([transforms.ToTensor()])
+        tensor = transform(image)
         tensor = tensor.to(self.device)
-        tensor = tensor[None]
-        tensor = tensor.permute(0,3,1,2)
+        #tensor = tensor.cuda()
+        tensor = torch.unsqueeze(tensor, 0)
         tensor = tensor.float()
         return tensor
 
@@ -32,22 +36,23 @@ class Line_Detection:
         use_cuda = not torch.cuda.is_available()
         self.device = torch.device("cuda" if use_cuda else "cpu")
         self.model = torch.load(path)
-        self.model.to(self.device)
+        #self.model = self.model.cuda()
+        self.model = self.model.to(self.device)
         self.model.eval()
 
 
 def main():
+    image_paths = glob.glob("/home/jetbot/Pictures/*.jpg")
 
     path = "./models/resnet18_95acc.pth" 
     ld = Line_Detection(path)
-    camera = CSICamera(width=224, height=224, capture_width=224, capture_height=224, capture_fps=21)
-    while True:
-        image = camera.read()
+    #camera = CSICamera(width=224, height=224, capture_width=224, capture_height=224, capture_fps=21)
+    for idx, img in enumerate(image_paths):
+        image = cv2.imread(img)
+        #image = camera.read()
         tensor = ld.preprocess(image)
         pred_class = ld.predict(tensor)
-        print(pred_class)
-        break
-        
+        print(img, idx, pred_class)
 
 if __name__=="__main__":
     main()
