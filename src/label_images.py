1f8235d77c756a2027368b492b9c98c3abc8571b
Phosfox <mordredur@googlemail.com>
2020-06-22 17:19:02 +0200

    label script

---
+++ src/label_images.py
@@ -0,0 +1,29 @@
+import glob
+import sys
+import cv2
+
+def main():
+    path = "C:\\Users\\mordr\\Downloads\\relabel"
+    img_paths = [p for p in glob.glob(f"{path}\\*\\*.jpg")]
+    for idx, img_path in enumerate(img_paths):
+        img = cv2.imread(img_path)
+        cv2.imshow("img", img)
+        key = cv2.waitKey(0)
+        if key == ord('w'):
+            straight_path = f"{path}\\straight\\{idx}.jpg"
+            print(straight_path)
+            cv2.imwrite(straight_path, img)
+        elif key == ord("a"):
+            left_path = f"{path}\\left\\{idx}.jpg"
+            print(left_path)
+            cv2.imwrite(left_path, img)
+        elif key == ord("d"):
+            right_path = f"{path}\\right\\{idx}.jpg"
+            print(right_path)
+            cv2.imwrite(right_path, img)
+        else:
+            print("else")
+            break
+
+if __name__ == "__main__":
+    main()
